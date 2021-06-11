4811ab5404d390a00f79c3344c64a901f69bdef2
claranggraini <claraanggraini_01@yahoo.com>
2021-06-11 12:09:19 +0700

    update validate achievement

--- Phosition/Congratulation/CongratulationController.swift
+++ Phosition/Congratulation/CongratulationController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+enum compType: String{
+    case rot = "Rule of Thirds"
+    case ll = "Leading Lines"
+    case ros = "Rule of Space"
+    case gr = "Golden Ratio"
+}
+
 class CongratulationController: UIViewController {
-    lazy var selectedComp: Composition = Database.shared.getCompositions()[1]
+    lazy var selectedComp: Composition = Database.shared.getCompositions()[2]
     lazy var compositions = Database.shared.getCompositions()
+    lazy var achievements = Database.shared.getAchievements()
     var achCount = 0
     
     @IBOutlet var congratulationView: CongratulationView!
@@ -53,9 +61,21 @@ class CongratulationController: UIViewController {
         if !firstTime{
             defaults.set(true, forKey: "firstTime")
             achCount+=1
+            achievements[4].isUnlocked = true
+            Database.shared.updateDatabase()
         }
         if selectedComp.prog == 1{
             achCount+=1
+            if selectedComp.title == compType.rot.rawValue{
+                achievements[0].isUnlocked = true
+            }else if selectedComp.title == compType.ll.rawValue{
+                achievements[1].isUnlocked = true
+            }else if selectedComp.title == compType.ros.rawValue{
+                achievements[2].isUnlocked = true
+            }else if selectedComp.title == compType.gr.rawValue{
+                achievements[3].isUnlocked = true
+            }
+            Database.shared.updateDatabase()
         }
     }
 }
